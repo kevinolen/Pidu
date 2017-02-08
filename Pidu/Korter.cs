fd8457516ead88ed435b6a7760e14fc626b8c1f3
Kevin Kukk <kevin.kukk>
2017-02-08 10:40:39 +0200

    lisan projekti failid

---
+++ Pidu/Korter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace Pidu
+{
+    class Korter
+    {
+        private int maksimumSuurus = 10;
+        private List<string> inimesed = new List<String>();
+
+        public void lisaInimene(string inimene)
+        {
+            if (kasMahubVeel()==true)
+            {
+                inimesed.Add(inimene);
+            }
+            else
+            {
+                var dialog = new MessageDialog("10 inimest on täis. Rohkem ei  mahu");
+                dialog.ShowAsync();
+            }
+        }
+        public void eemaldaInimene(string inimene)
+        {
+            inimesed.Remove(inimene);
+        }
+
+        public Boolean kasMahubVeel()
+        {
+            Boolean kontroll = false;
+
+            if(inimesed.Count < maksimumSuurus)
+            {
+                kontroll = true;
+            }
+            else
+            {
+                kontroll = false;
+            }
+
+            return kontroll;
+        }
+
+        public string kuvaInimesed()
+        {
+            string rahvas = "";
+
+            for (int i = 0; i < inimesed.Count; i++)
+            {
+                rahvas = rahvas + inimesed[i] + System.Environment.NewLine;
+            }
+
+            return rahvas;
+        }
+
+    }
+}
---
+++ Pidu/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Pidu
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Korter korter1 = new Korter();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void LisaButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(TextBox1.Text))
+            {
+            korter1.lisaInimene(TextBox1.Text);
+            TextBox1.Text = "";
+            TextBlock1.Text = korter1.kuvaInimesed();  
+            }
+            else
+            {
+                var dialog = new MessageDialog("TextBox ei tohi olla tühi!");
+                dialog.ShowAsync();
+            }
+        }
+
+        private void EemaldaButton_Click(object sender, RoutedEventArgs e)
+        {
+            korter1.eemaldaInimene(TextBox1.Text);
+            TextBox1.Text = "";
+            TextBlock1.Text = korter1.kuvaInimesed();
+
+        }
+    }
+}
